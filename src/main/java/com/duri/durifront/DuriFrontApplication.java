0d8d89ed68102d2d0697cb51f884f1c4516cac70
YeonWooKimm <kyw10987@gmail.com>
2026-02-03 21:09:57 +0900

    initial commit

---
+++ src/main/java/com/duri/durifront/DuriFrontApplication.java
@@ -0,0 +1,13 @@
+package com.duri.durifront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuriFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuriFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duri/durifront/DuriFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duri.durifront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuriFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
