fba5d4949e8a38210d30c5805b68e363d3eb7121
chaewonn <scw0629@naver.com>
2026-03-28 22:10:57 +0900

    feat: JWT 인증 필터, 회원가입 로직, 회원가입/로그인 화면 구현 완료 (#6)
    * feat: JWT 인증 필터 구현 완료
    
    * feat: 회원가입 로직 및 API 구현 및 테스트 완료
    
    * feat: auth 화면 구현 및 테스트 완료
    
    * refactor: 회원가입 시 입력값 검증 로직 추가 및 화면 수정 완료

---
+++ src/main/java/com/duri/durifront/auth/exception/AuthErrorCode.java
@@ -0,0 +1,25 @@
+package com.duri.durifront.auth.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCode implements BaseErrorCode {
+
+    // 401 Unauthorized : Token
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "FRONT-AUTH-401-1", "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "FRONT-AUTH-401-2", "토큰이 만료되었습니다."),
+
+    // 403 Forbidden
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "FRONT-AUTH-403-1", "접근 권한이 없습니다."),
+
+    // 500 Internal Server Error
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "FRONT-AUTH-500-1", "서버 내부 오류가 발생했습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+}
--- src/main/java/com/duri/durifront/chat/dto/response/ChatMessageSliceViewDTO.java
+++
@@ -1,9 +0,0 @@
-package com.duri.durifront.chat.dto.response;
-
-import java.util.List;
-
-public record ChatMessageSliceViewDTO(
-        List<ChatMessageGroupResponseDTO> messages,
-        boolean hasNext
-) {
-}
