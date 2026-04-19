00dbe2060fef3e7a133917e8ccb3cbb5ede0f6c4
YeonWooKimm <kyw10987@gmail.com>
2026-04-19 21:36:52 +0900

    fix:회원가입에 왜 bcrypt추가를..

--- src/main/java/com/duri/durifront/user/service/UserService.java
+++ src/main/java/com/duri/durifront/user/service/UserService.java
@@ -3,24 +3,33 @@ package com.duri.durifront.user.service;
 import com.duri.durifront.user.entity.User;
 import com.duri.durifront.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User createUser(String username,
                            String rawPassword,
                            String email
     )
     {
-        // TODO: PasswordEncoder 적용
-        User user = User.createUser(username, rawPassword, email);
+        String encodedPassword = bCryptPasswordEncoder.encode(rawPassword);
+        User user = User.createUser(username, encodedPassword, email);
+
         return userRepository.save(user);
     }
 
     public boolean isUsernameDuplicated(String username) {
         return userRepository.existsByUsername(username);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
